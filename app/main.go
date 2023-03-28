8b9203110c7ff44bdbe38c789dafd6cba31aa959
Habibmahaasin <habibirf23@gmail.com>
2023-03-28 07:56:38 +0700

    add temporarry error hendling

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"SmartAerators/infrastructures/config"
 	"SmartAerators/infrastructures/database"
 	"SmartAerators/routes/v1"
+	"errors"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -15,6 +17,26 @@ func Define() (*fiber.App, database.Database) {
 	engine := html.New("./public/v1/templates", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			code := fiber.StatusInternalServerError
+
+			var e *fiber.Error
+			if errors.As(err, &e) {
+				code = e.Code
+			}
+
+			err = ctx.Status(code).Render(fmt.Sprintf("errors/%d", code), fiber.Map{
+				"Title": "Error " + fmt.Sprintf("%d", code),
+			})
+
+			if err != nil {
+				return ctx.Status(500).Render("errors/500", fiber.Map{
+					"Title": fiber.ErrInternalServerError.Message,
+				})
+			}
+
+			return nil
+		},
 	})
 
 	app.Use(logger.New(logger.Config{
